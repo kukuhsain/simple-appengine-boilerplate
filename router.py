d30b07eeaf2a0902266e35f4fb23f93a5c23d2d9
Kukuh <kukuhsain@gmail.com>
2016-12-31 18:01:56 +0700

    Update router by adding url path for user auth controllers

--- router.py
+++ router.py
@@ -1,7 +1,11 @@
 import webapp2
 
 from controller.home_controller import Home
+from controller.user_auth_controller import UserRegister, UserLogin, UserLogout
 
 app = webapp2.WSGIApplication([
-    ("/", Home)
+    ("/", Home),
+    ("/api/v1/auth/register", UserRegister),
+    ("/api/v1/auth/login", UserLogin),
+    ("/api/v1/auth/logout", UserLogout),
 ], debug=True)
